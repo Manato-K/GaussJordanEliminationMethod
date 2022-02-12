92093deb900768709bfa2538fb3e099ef53486e0
chrom-P <kkllmm104@gmail.com>
2022-02-13 03:48:34 +0900

    up

--- Gauss-Jordan-limination-method.c
+++ Gauss-Jordan-limination-method.c
@@ -6,9 +6,9 @@
 int main(void)
 {
     int i,j,k;
-    float A[N][N+1],q;
+    float A[N][N+1],q,b;
     FILE *fp;
-    if( (fp=fopen("20191105-3.dat","r"))==NULL ) {
+    if( (fp=fopen("text.txt","r"))==NULL ) {
 
     printf("cannot open file.\n");
     exit(0);
@@ -24,6 +24,11 @@ int main(void)
     printf("\n");
     for(i=0;i<N;i++) {
         if(A[i][i]==0.0) break;
+        for(j=i;j<N+1;j++){
+            b=A[i][j];
+            A[i][j]=A[i+1][j];
+            A[i+1][j]=b;}
+        }
         q=A[i][i];
         for(j=i;j<N+1;j++) A[i][j]/=q;
         for(k=0;k<N;k++) {
@@ -32,9 +37,19 @@ int main(void)
             for(j=i;j<N+1;j++) A[k][j]-=q*A[i][j];
             }
         }
-    }
     for(i=0;i<N;i++) {
         for(j=0;j<N+1;j++) printf("%6.2f",A[i][j]);
         printf("\n");
     }
 }
+/*
+   5.00  -2.00   2.00   1.00  -1.00
+   3.00  -1.00  -1.00  -2.00  -2.00
+   6.00  -3.00   0.00   0.00   0.00
+   0.00   0.00   0.00   0.00   0.00
+
+  3.00 -1.00 -1.00 -2.00  0.00
+  5.00 -3.00  0.00  0.00  0.00
+  6.00 -2.00  0.00  0.00  0.00
+  0.00  0.00  2.00  0.00  0.00
+*/
